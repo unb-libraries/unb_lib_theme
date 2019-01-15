adeb1cb9c24f59cfb332dfbb2909a8441b6ca6ec
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-01-15 16:31:58 -0400

    LIB-1 Barrio+Bootstrap 4.1.3 initial commit

---
+++ src/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.unb_lib_theme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
