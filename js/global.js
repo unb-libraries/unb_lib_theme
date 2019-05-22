7e83df6676fe113f0b4ebe153e648bd23ddb65c2
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-05-22 13:16:00 -0300

    LIB-73 Header nav affix fix (port js from lib_unb_ca)

--- js/global.js
+++ js/global.js
@@ -10,13 +10,19 @@
   Drupal.behaviors.unb_lib_theme = {
     attach: function (context, settings) {
       var header = $("#navbar-main");
+      var header_logo = $("#navbar-main .branding > *");
+      var header_nav_buttons = $("#navbar-main .megamenu-li > a");
       $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         var width = $(window).innerWidth();
         if (scroll >= 130 && width >= 991) {
           header.addClass("affix");
+          header_nav_buttons.removeClass("py-lg-4");
+          header_logo.removeClass("py-2");
         } else {
           header.removeClass("affix");
+          header_nav_buttons.addClass("py-lg-4");
+          header_logo.addClass("py-2");
         }
       });
     }
