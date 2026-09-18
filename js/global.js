5ecb40a512a5c39da9a21d7a7006143937b86245
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2026-09-18 14:16:22 -0300

    LIB-712 Cleanup/update global JS

--- js/global.js
+++ js/global.js
@@ -10,9 +10,7 @@
   Drupal.behaviors.unb_lib_theme = {
     attach: function (context, settings) {
       var banner = $("#banner");
-      var header = $("#navbar-main");
-      var header_logo = $("#navbar-main .branding > *");
-      var header_nav_buttons = $("#navbar-main .megamenu-li > a");
+      var header = $("#navbar-top");
 
       $(window).resize(function () {
         setBanner(banner);
@@ -20,14 +18,10 @@
 
       $(window).scroll(function () {
         let scroll = $(window).scrollTop();
-        if (scroll >= 130 && window.width >= 977) {
+        if (scroll >= 131 && window.width >= 977) {
           header.addClass("affix");
-          // header_nav_buttons.removeClass("py-lg-4");
-          // header_logo.removeClass("py-2");
         } else {
           header.removeClass("affix");
-          // header_nav_buttons.addClass("py-lg-4");
-          // header_logo.addClass("py-2");
         }
       });
       setBanner(banner);
